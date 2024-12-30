22f82dfdeef0805c42eb99d269b339b4a1d1b1a3
shahid66 <kk.shahid66@gmail.com>
2024-12-30 21:21:05 +0600

    second

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,3 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
 import express, { Application } from 'express';
 import globalErrorHandler from './app/middlewares/globalErrorhandler';
 import notFound from './app/middlewares/notFound';
@@ -13,22 +7,16 @@ const app: Application = express();
 
 //parsers
 app.use(express.json());
-app.use(cookieParser());
-app.use(cors({ origin: ['http://localhost:5173'] }));
+
+app.get('/', (req, res) => {
+  res.send('welcome');
+});
 
 // application routes
 app.use('/api/v1', router);
 
-// const test = async (req: Request, res: Response) => {
-//   const a = 10;
-//   res.send(a);
-// };
-
-// app.get('/', test);
-
 app.use(globalErrorHandler);
 
-//Not Found
 app.use(notFound);
 
 export default app;
